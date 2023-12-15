d54cd3e578b349947f920a1ff027544ac871e8ce
Lucas Cercal <lucascercal.fontes@gmail.com>
2023-12-15 13:33:53 -0300

    feat: add tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 // components
 import Header from './components/Header';
 import Footer from './components/Footer';
@@ -11,12 +13,14 @@ import styles from './App.module.css'
 import { ITask } from './interfaces/Task';
 
 function App() {
+  const [taskList, setTaskList] = useState<ITask[]>([])
+
   return <div>
     <Header />
     <main className={styles.main}>
       <div>
         <h1>Lorem Ipsum is simply dummy text of the printing and typesetting industry...</h1>
-        <TaskForm btnText='Criar tarefa'/>
+        <TaskForm btnText='Criar tarefa' taskList={taskList} setTaskList={setTaskList}/>
       </div>
       <div>
         <h2>Suas tarefas</h2>
