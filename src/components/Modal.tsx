617ede86fbb0c39707ea24e1b36995e170fb3a15
Lucas Cercal <lucascercal.fontes@gmail.com>
2023-12-18 09:56:17 -0300

    feat: add modal

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,20 +1,19 @@
-import React from 'react'
+import { ReactNode } from 'react'
 
 // css
 import styles from './Modal.module.css'
 
 interface Props {
-
+  children: ReactNode
 }
 
-const Modal = (props: Props) => {
+const Modal = ({ children }: Props) => {
   return (
     <div id='modal'>
-      <div>
-
-      </div>
-      <div>
+      <div className={styles.fade}></div>
+      <div className={styles.modal}>
         <h2>Texto Modal</h2>
+        {children}
       </div>
     </div>
   )
